d7ba98a215c57daaf10fbce95d7d2c8f88b39fca
Michael Kick <m.kick@proventa.de>
2015-07-04 13:19:57 +0200

    Revert "Revert "structure update!""
    This reverts commit cb53def6fd69ce0014cea247603b1421d094899e.

--- www/js/app.js
+++ www/js/app.js
@@ -38,12 +38,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.map', {
+    url: '/map',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        //controller: 'DashCtrl'
+      'tab-map': {
+        templateUrl: 'templates/tab-map.html',
+        controller: 'mapCoordinates'
       }
     }
   })
@@ -51,8 +51,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   .state('tab.chats', {
       url: '/chats',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
+        'tab-teams': {
+          templateUrl: 'templates/tab-teams.html',
           controller: 'ChatsCtrl'
         }
       }
@@ -70,15 +70,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   .state('tab.info', {
     url: '/info',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-info.html',
+      'tab-info': {
+        templateUrl: 'templates/tab-info.html'
         //controller: 'InfoCtrl'
       }
     }
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/map');
 
 });
 
